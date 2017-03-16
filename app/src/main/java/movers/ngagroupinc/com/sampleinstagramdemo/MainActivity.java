fea788dd7216d90ec8f007988d65f32bada668e2
uma kondeti <kondetiuma@gmail.com>
2017-03-16 06:52:17 -0400

    Update MainActivity.java
    added user extra information on instagram

--- app/src/main/java/movers/ngagroupinc/com/sampleinstagramdemo/MainActivity.java
+++ app/src/main/java/movers/ngagroupinc/com/sampleinstagramdemo/MainActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+Bundlebundle;
+TextView user_name,user_age,user_location;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
